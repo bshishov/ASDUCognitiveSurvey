2e950566ff3f6a3bde63fae10eda52699aeeea05
Boris Shishov <borisshishov@gmail.com>
2019-09-08 20:34:46 +0300

    This god damn naming in json library

--- processors/utils/main.py
+++ processors/utils/main.py
@@ -7,7 +7,7 @@ def default_main(fn: callable):
     input_filename = args[0]
     output_filename = args[1]
     with open(input_filename, 'r', encoding='utf-8') as input_file:
-        input_data = json.loads(input_file)
+        input_data = json.load(input_file)
         output_data = fn(**input_data)
         with open(output_filename, 'w', encoding='utf-8') as output_file:
             json.dump(output_data, output_file)
