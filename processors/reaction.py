d00fbeb7ac6e78eb4a5324a297332df0e7e5ca8f
Boris Shishov <borisshishov@gmail.com>
2020-08-07 00:27:47 +0300

    Reaction fix

--- processors/reaction.py
+++ processors/reaction.py
@@ -5,12 +5,19 @@ MIN_REACTION = 250.0  # ms
 MAX_REACTION = 500.0  # ms
 
 
+def _get_rt(_e):
+        _args = _e['args']
+        if isinstance(_args, int):
+            return _args
+        return _args['reaction']
+
+
 def reaction(text_data, **kwargs) -> dict:
     # files
     # text_data
     events = json.loads(text_data['events'])
 
-    reactions = [e['args']['reaction'] for e in events if e['name'] == 'success']
+    reactions = [_get_rt(e) for e in events if e['name'] == 'success']
 
     # Requested marks
     fails = len([e for e in events if e['name'] == 'fail'])
